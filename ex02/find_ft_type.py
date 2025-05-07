b2d59fb135f5b5d8f520148f36228618d8299374
user <user@MacBook-Pro-von-user.local>
2025-05-07 18:30:58 +0200

    ex02

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -1,3 +1,20 @@
 def all_thing_is_obj(object: any) -> int:
-    print(type(object))
+    obj_type = type(object)
+    if obj_type == list:
+        print(f"List : {obj_type}")
+    elif obj_type == tuple:
+        print(f"Tuple : {obj_type}")
+    elif obj_type == set:
+        print(f"Set : {obj_type}")
+    elif obj_type == dict:
+        print(f"Dict : {obj_type}")
+    elif obj_type == str:
+        if object == "Brian":
+            print(f"Brian is in the kitchen {obj_type}")
+        elif object == "Toto":
+            print(f"Toto is in the kitchen : {obj_type}")
+        else:
+            print(f"Str : {obj_type}")
+    else:
+        print("Type not found")
     return 42
